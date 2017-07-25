1c2015d317c89f6f5ae64cd737aabb41549235e5
aformacsabrina <boule.sabr@gmail.com>
2017-07-25 09:48:34 +0200

    mise en place du test.

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -1,26 +1,35 @@
 var demande = prompt("Veuillez deviner un chiffre entre 10 et 20.");
-function getRandomIntInclusive(min,max) {
+function getRandomIntInclusive(min, max) {  
     min = Math.ceil(10);
     max = Math.floor(21);
-    return  Math.floor(Math.random() * (max - min +1)) + min;
+    return  Math.floor(Math.random() * (max - min + 1)) + min;
 }
-/*appel de fonction*/
-if (demande <= function getRandomIntInclusive)
+alert(getRandomIntInclusive(10, 21));
+if (demande < getRandomIntInclusive(10, 21)) {
+    
+    prompt("Votre chiffre est trop petit!");
+} else if (demande > getRandomIntInclusive(10, 21)) {
+    promt("Votre chiffre est trop grand!");
+} else if (demande === getRandomIntInclusive(10, 21)) {
+    
+    prompt("Bravo vous avez gagné!");
+}
+
 /*if (demande <= 9) {
-    alert("Veuillez entrer un chiffre entre 10 et 20.");
-    prompt("Veuillez choisir un chiffre entre 10 et 20.");
-}
-if (demande > 16) {
-    alert("Votre chiffre est inférieur à la bonne réponse.");
-    prompt("Veuillez choisir un chiffre entre 10 et 20.");
-}
-if (demande < 16) {
-    alert("Votre chiffre est supérieur à la bonne réponse.");
-    prompt("Veuillez choisir un chiffre entre 10 et 20.");
-}
-if (demande === 16) {
-    alert("Bravo vous avez gagné!");
-}*/
+ alert("Veuillez entrer un chiffre entre 10 et 20.");
+ prompt("Veuillez choisir un chiffre entre 10 et 20.");
+ }
+ if (demande > 16) {
+ alert("Votre chiffre est inférieur à la bonne réponse.");
+ prompt("Veuillez choisir un chiffre entre 10 et 20.");
+ }
+ if (demande < 16) {
+ alert("Votre chiffre est supérieur à la bonne réponse.");
+ prompt("Veuillez choisir un chiffre entre 10 et 20.");
+ }
+ if (demande === 16) {
+ alert("Bravo vous avez gagné!");
+ }*/
 
 
 
