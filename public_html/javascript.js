1e7dbbcdf216d1bff2909d4db7ed7d049ec846a7
aformacsabrina <boule.sabr@gmail.com>
2017-07-25 09:24:51 +0200

    ajout de commentaire

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -1,5 +1,12 @@
 var demande = prompt("Veuillez deviner un chiffre entre 10 et 20.");
-if (demande <= 9) {
+function getRandomIntInclusive(min,max) {
+    min = Math.ceil(10);
+    max = Math.floor(21);
+    return  Math.floor(Math.random() * (max - min +1)) + min;
+}
+/*appel de fonction*/
+if (demande <= function getRandomIntInclusive)
+/*if (demande <= 9) {
     alert("Veuillez entrer un chiffre entre 10 et 20.");
     prompt("Veuillez choisir un chiffre entre 10 et 20.");
 }
@@ -13,7 +20,7 @@ if (demande < 16) {
 }
 if (demande === 16) {
     alert("Bravo vous avez gagné!");
-}
+}*/
 
 
 
